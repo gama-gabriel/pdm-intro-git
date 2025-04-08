3e89313b58c9a2921460733bb6de453d9cfc1e85
gabriel gama <bielmgama@gmail.com>
2025-04-07 23:01:08 -0300

    test: testa a divisão

--- intro_git/teste_calculadora.py
+++ intro_git/teste_calculadora.py
@@ -6,3 +6,4 @@ b = 2
 print(f'{a} + {b} = {calculadora.somar(1,2)}')
 print(f'{a} - {b} = {calculadora.subtrair(1,2)}')
 print(f'{a} * {b} = {calculadora.multiplicar(1,2)}')
+print(f'{a} / {b} = {calculadora.dividir(1,2)}')
